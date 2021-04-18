75e183bb6fedd547439e27487d3c49822594eedc
Palinus <chaospali@gmail.com>
2021-04-18 23:11:16 +0200

    BG start + fps limit
     fps limit added to gamemanager

--- BulletHellJam/Assets/Scripts/GameManager.cs
+++ BulletHellJam/Assets/Scripts/GameManager.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
+
+	public int targetFPS = 1000;
 	private void Awake() {
 		Physics.gravity = Vector3.zero;
+		Application.targetFrameRate = targetFPS; // pali was here
 	}
 }
