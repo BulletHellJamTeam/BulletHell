2a430b028f73bbd74b2e9656093844916a99fd37
2020wmarvil <2020wmarvil@gmail.com>
2021-04-21 00:00:02 -0400

    Spawning system

--- BulletHellJam/Assets/Scripts/EnemySpawner.cs
+++ BulletHellJam/Assets/Scripts/EnemySpawner.cs
@@ -1,12 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour {
+
+    public enum SpawnState { SPAWNING, WAITING, COUNTING };
+
+    [System.Serializable]
+    public class Wave {
+        public string name;
+        public Transform enemy;
+        public int count;
+        public float rate;
+    }
+
+    public Wave[] waves;
+
+    private int nextWave = 0;
     private float timeBetweenWaves = 5f;
-    private int currentWave;
+    private float waveCountdown;
+    private SpawnState state = SpawnState.COUNTING;
 
     private void Start() {
-        currentWave = 1;
+        waveCountdown = timeBetweenWaves;
+    }
+
+    private void Update() {
+        if (waveCountdown <= 0) {
+            if (state != SpawnState.SPAWNING) {
+                StartCoroutine(SpawnWave(waves[nextWave]));
+            } else {
+                waveCountdown -= Time.deltaTime;
+            }
+        }
+    }
+
+    IEnumerator SpawnWave(Wave wave) {
+        state = SpawnState.SPAWNING;
+
+        for (int i=0; i<wave.count; i++) {
+            SpawnEnemy(wave.enemy);
+            yield return new WaitForSeconds(1f / wave.rate);
+        }
+
+        yield break;
+    }
+
+    private void SpawnEnemy(Transform enemy) {
+        Debug.Log("spawning enemy: " + enemy.name);
     }
 }
