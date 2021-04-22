f4f0db39fd2607ac2810ce050156f72b9b35ed7c
2020wmarvil <2020wmarvil@gmail.com>
2021-04-21 23:45:11 -0400

    Dash

--- BulletHellJam/Assets/Scripts/PlayerController.cs
+++ BulletHellJam/Assets/Scripts/PlayerController.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using EZCameraShake;
+using TMPro;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour {
+    public enum PlayerState { MOVING, DASHING };
+
 	[SerializeField] private Animator animRef;
 	private Rigidbody rigidBodyRef;
 
@@ -13,24 +18,62 @@ public class PlayerController : MonoBehaviour {
 	private float slowdownDuration = 0.5f;
 	private float slowdownTimeElapsed = 0f;
 
+	// dashing variables
+	[SerializeField] private GameObject echoPrefab;
+	private float dashSpeed = 100f;
+	private float dashTime = 0.05f;
+	private float dashTimer = 0f;
+	private float dashBackTime = 2.5f;
+	private float dashBackTimer = 0f;
+	private bool justDashed = false;
+	private float dashEchoTime = 0.001f;
+	private float dashEchoTimer = 0f;
+
+	private PlayerState state = PlayerState.MOVING;
+	private PlayerState oldState = PlayerState.MOVING;
+
 	private void Awake() {
 		rigidBodyRef = gameObject.GetComponent<Rigidbody>();
 	}
 
 	private void FixedUpdate() {
-		Vector3 inputMovement = rawInputMovement.normalized * playerSpeed;
+		if (state == PlayerState.MOVING) {
+			Vector3 inputMovement = rawInputMovement.normalized * playerSpeed;
 
-		rigidBodyRef.AddForce(inputMovement);
+			rigidBodyRef.AddForce(inputMovement);
 
-		if(rigidBodyRef.velocity.magnitude > maxSpeed){
-             rigidBodyRef.velocity = Vector3.ClampMagnitude(rigidBodyRef.velocity, maxSpeed);
-        }
+			if(rigidBodyRef.velocity.magnitude > maxSpeed){
+			     rigidBodyRef.velocity = Vector3.ClampMagnitude(rigidBodyRef.velocity, maxSpeed);
+			}
 
-		if (inputMovement == Vector3.zero && slowdownTimeElapsed < slowdownDuration) {
-			rigidBodyRef.velocity = Vector3.Lerp(rigidBodyRef.velocity, Vector3.zero, slowdownTimeElapsed / slowdownDuration);
-			slowdownTimeElapsed += Time.fixedDeltaTime;
-		} else {
-			slowdownTimeElapsed = 0f;
+			if (inputMovement == Vector3.zero && slowdownTimeElapsed < slowdownDuration) {
+				rigidBodyRef.velocity = Vector3.Lerp(rigidBodyRef.velocity, Vector3.zero, slowdownTimeElapsed / slowdownDuration);
+				slowdownTimeElapsed += Time.fixedDeltaTime;
+			} else {
+				slowdownTimeElapsed = 0f;
+			}
+		} else if (state == PlayerState.DASHING) {
+			dashTimer += Time.fixedDeltaTime;
+			dashEchoTimer += Time.fixedDeltaTime;
+
+			if (dashEchoTimer > dashEchoTime) {
+				GameObject echo = (GameObject)Instantiate(echoPrefab, rigidBodyRef.transform.position, Quaternion.identity);
+				Destroy(echo, 0.5f);
+
+				dashEchoTimer = 0f;
+			}
+			
+			if (dashTimer > dashTime) {
+				rigidBodyRef.velocity = Vector3.zero;
+
+				state = oldState;
+			}
+		}
+
+		if (justDashed) {
+			dashBackTimer += Time.fixedDeltaTime;
+
+			if (dashBackTimer > dashBackTime) justDashed = false;
 		}
 
 		animRef.SetFloat("VelocityX", rigidBodyRef.velocity.x);
@@ -52,5 +95,25 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	public void OnDash(InputAction.CallbackContext value) {
+		if (value.started && state != PlayerState.DASHING) Dash();
+	}
+
+	private void Dash() {
+		oldState = state;
+		state = PlayerState.DASHING;
+
+		dashTimer = 0f;
+		dashEchoTimer = 0f;
+
+		CameraShaker.Instance.ShakeOnce(2f, 2f, 0.1f, 0.5f);
+
+		if (justDashed) {
+			rigidBodyRef.velocity = Vector3.left * dashSpeed;
+			justDashed = false;
+		} else {
+			rigidBodyRef.velocity = Vector3.right * dashSpeed;
+			dashBackTimer = 0f;
+			justDashed = true;
+		}
 	}
 }
