911393f834368b4b75926d16526ebc5ab9300421
Wyatt Marvil <2020wmarvil@gmail.com>
2021-04-22 01:50:34 -0400

    Bat movement

--- BulletHellJam/Assets/Scripts/BatController.cs
+++ BulletHellJam/Assets/Scripts/BatController.cs
@@ -1,8 +1,77 @@
 using UnityEngine;
 
 public class BatController : MonoBehaviour {
-	[SerializeField] private Animator animReference;
+    public enum BatState { ENTERING, FIGHTING };
 
-	private void Awake() {
-	}
+    // timers
+    private float newPathTimeMin = 0.2f, newPathTimeMax = 1.5f, newPathTime = 0f;
+    private float newPathTimer = 0f;
+    private float attackTimeMin = 1f, attackTimeMax = 3f, attackTime = 1.5f;
+    private float attackTimer = 0f;
+
+    // bounds
+	private float LeftMoveBound, RightMoveBound, BottomMoveBound, TopMoveBound;
+
+    // movement data
+    private Rigidbody rigidbodyRef;
+    private Vector3 targetPos;
+    private float batSpeed = 5f;
+
+    BatState state = BatState.ENTERING;
+
+    private void Awake() {
+        rigidbodyRef = GetComponent<Rigidbody>();
+        targetPos = rigidbodyRef.transform.position;
+    }
+
+    private void Start() {
+		LeftMoveBound = Camera.main.ViewportToWorldPoint(new Vector3(0.25f, 0f, 0f)).x;
+		RightMoveBound = Camera.main.ViewportToWorldPoint(new Vector3(0.75f, 0f, 0f)).x;
+
+		BottomMoveBound = Camera.main.ViewportToWorldPoint(new Vector3(0f, 0.25f, 0f)).y;
+		TopMoveBound = Camera.main.ViewportToWorldPoint(new Vector3(0f, 0.75f, 0f)).y;
+
+        newPathTimer = newPathTime;
+    }
+
+    private void FixedUpdate() {
+        newPathTimer += Time.fixedDeltaTime;
+
+        // every x seconds, path to a new random place within bounds
+        if (newPathTimer > newPathTime) {
+            // set a new destination
+            targetPos = new Vector3(
+                Random.Range(LeftMoveBound, RightMoveBound),
+                Random.Range(BottomMoveBound, TopMoveBound),
+                0f
+                );
+
+            print(targetPos);
+
+            newPathTimer = 0f;
+            newPathTime = Random.Range(newPathTimeMin, newPathTimeMax);
+        }
+
+        // move bat!
+        Vector3 moveDir = targetPos - rigidbodyRef.transform.position;
+        Vector3 moveDist = moveDir.normalized * batSpeed * Time.fixedDeltaTime;
+
+        if (moveDir.magnitude > 0.5f) //rigidbodyRef.MovePosition(rigidbodyRef.transform.position + moveDist);
+            rigidbodyRef.AddForce(moveDist);
+
+        if (state != BatState.FIGHTING) return;
+
+        attackTimer += Time.fixedDeltaTime;
+
+        // every y seconds use one of two different basic attacks
+        if (attackTimer > attackTime) {
+            // atk1 - fire three projectiles at player
+            // atk2 - fire a circle of projectiles in all directions
+
+            print("attack");
+
+            attackTimer = 0f;
+            attackTime = Random.Range(attackTimeMin, attackTimeMax);
+        }
+    }
 }
--- BulletHellJam/Assets/Scripts/EnemySpawner.cs
+++ BulletHellJam/Assets/Scripts/EnemySpawner.cs
@@ -1,11 +1,9 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour {
 
-    public enum SpawnState { SPAWNING, WAITING, COUNTING };
+    public enum SpawnState { SPAWNING, WAITING, COUNTING, GAME_OVER };
 
     [System.Serializable]
     public class Wave {
@@ -16,27 +14,39 @@ public class EnemySpawner : MonoBehaviour {
     }
 
     public Wave[] waves;
+    public Transform[] spawnPoints;
 
     private int nextWave = 0;
-    private float timeBetweenWaves = 5f;
+    private float timeBetweenWaves = 2.5f;
     private float waveCountdown;
     private SpawnState state = SpawnState.COUNTING;
+    private float searchCountdown = 1f;
 
     private void Start() {
         waveCountdown = timeBetweenWaves;
     }
 
     private void Update() {
+        if (state == SpawnState.GAME_OVER) return;
+
+        if (state == SpawnState.WAITING) {
+            if (!EnemyIsAlive()) {
+                WaveCompleted();
+            } else { 
+                return;
+            }
+        }
+
         if (waveCountdown <= 0) {
             if (state != SpawnState.SPAWNING) {
                 StartCoroutine(SpawnWave(waves[nextWave]));
-            } else {
-                waveCountdown -= Time.deltaTime;
             }
+        } else {
+            waveCountdown -= Time.deltaTime;
         }
     }
 
-    IEnumerator SpawnWave(Wave wave) {
+    private IEnumerator SpawnWave(Wave wave) {
         state = SpawnState.SPAWNING;
 
         for (int i=0; i<wave.count; i++) {
@@ -47,7 +57,34 @@ public class EnemySpawner : MonoBehaviour {
         yield break;
     }
 
+    private bool EnemyIsAlive() {
+        searchCountdown -= Time.deltaTime;
+        if (searchCountdown <= 0f) {
+            searchCountdown = 1f;
+            if (GameObject.FindGameObjectWithTag("Bat") == null) {
+                return false;
+            }
+        } return true;
+
+    }
+
+    private void WaveCompleted() {
+        state = SpawnState.COUNTING;
+        waveCountdown = timeBetweenWaves;
+
+        nextWave++;
+
+        if (nextWave >= waves.Length)
+        {
+            state = SpawnState.GAME_OVER;
+        }
+    }
+
     private void SpawnEnemy(Transform enemy) {
-        Debug.Log("spawning enemy: " + enemy.name);
+        print("spawning enemy: " + enemy.name);
+
+        Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+
+        Instantiate(enemy, spawnPoint.position, Quaternion.identity);
     }
 }
